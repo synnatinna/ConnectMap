e5d8fc7ddbbb9c292a96a35385609557543b1d70
synnatinna <synnatinna@gmail.com>
2017-04-20 11:49:42 +0700

    Test Map

--- app/src/main/java/sdu/comsci/synna/connectmap/CallMap.java
+++ app/src/main/java/sdu/comsci/synna/connectmap/CallMap.java
@@ -1,7 +1,11 @@
 package sdu.comsci.synna.connectmap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMap extends AppCompatActivity {
 
@@ -11,4 +15,22 @@ public class CallMap extends AppCompatActivity {
         setContentView(R.layout.activity_call_map);
     }   //onCreate Method
 
+        public void runCallMap(View view){
+            //intitial view
+            EditText latitude = (EditText) findViewById(R.id.mapLat);
+            EditText longtitude = (EditText) findViewById(R.id.mapLong);
+            EditText label = (EditText) findViewById(R.id.mapLabel);
+
+            String lat = latitude.getText().toString().trim();
+            String lng = longtitude.getText().toString().trim();
+            String mlabel = label.getText().toString().trim();
+
+
+            Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:13.759558, 100.541525(ศูนย์รางน้ำ)");
+
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+           // mapIntent.setPackage("com.google.android.apps.maps"); //ใช้กับ genymotion ไม่ได้
+            startActivity(mapIntent);
+
+        }// runCallMap
 }   //Main Class
