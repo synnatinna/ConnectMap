c7feec01eeacc47a05181723e53e096c8805aa84
synnatinna <synnatinna@gmail.com>
2017-04-20 10:05:57 +0700

    Design UI

---
+++ app/src/main/java/sdu/comsci/synna/connectmap/CallMap.java
@@ -0,0 +1,14 @@
+package sdu.comsci.synna.connectmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_map);
+    }   //onCreate Method
+
+}   //Main Class
